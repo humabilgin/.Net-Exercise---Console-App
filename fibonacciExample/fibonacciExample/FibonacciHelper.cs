9ab7b6434de9d2c6fa07c34f7d653728d5768ab8
Hüma Bilgin <46404802+humabilgin@users.noreply.github.com>
2022-05-18 18:32:15 +0300

    Add files via upload

---
+++ fibonacciExample/fibonacciExample/FibonacciHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciExample
+{
+    public class FibonacciHelper
+    {
+        public bool IsFibonacci(int number)
+        {
+            bool isFibonacciNumber = false;
+            int pastFibonacciNumber = 1;
+            int secondPastFibonacciNumber = 0;
+            int currentFibonacciNumber;
+
+            if (number == secondPastFibonacciNumber)
+            {
+                isFibonacciNumber = true;
+            }
+            while (true)
+            {
+                currentFibonacciNumber = pastFibonacciNumber + secondPastFibonacciNumber;
+
+                if (currentFibonacciNumber > number)
+                {
+                    break;
+                }
+                else if (number == currentFibonacciNumber)
+                {
+                    isFibonacciNumber = true;
+                    break;
+                }
+                secondPastFibonacciNumber = pastFibonacciNumber;
+                pastFibonacciNumber = currentFibonacciNumber;
+
+            }
+            return isFibonacciNumber;
+        }
+
+        public int FindBiggestNumber(List<int> evenNumbers)
+        {
+            evenNumbers.Sort();
+            evenNumbers.Reverse();
+            Console.Write("\nSorted even numbers : ");
+            for (int i = 0; i < evenNumbers.Count; i++)
+                Console.Write(evenNumbers[i] + " ");
+
+            Console.WriteLine("\nBiggest Number : {0}", evenNumbers[0]);
+            return evenNumbers[0];
+        }
+    }
+}
---
+++ fibonacciExample/fibonacciExample/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fibonacciExample
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
+            FibonacciHelper fibonacci = new FibonacciHelper();
+
+            //flow
+            numbers = ReadInputAndFindNumbers();
+            evenNumbers = FindEvenNumbers(numbers);
+            int biggestNumber = fibonacci.FindBiggestNumber(evenNumbers);
+            bool isFibonacciNumber = fibonacci.IsFibonacci(biggestNumber);
+            //end of the flow
+            
+            if (isFibonacciNumber)
+            {
+                Console.WriteLine("\n\n************The biggest even number is a Fibonacci number.************");
+            }
+            else
+            {
+                Console.WriteLine("\n\n************The biggest even number is not a Fibonacci number.************");
+            }
+        }
+        static List<int> ReadInputAndFindNumbers()
+        {
+
+            int sum = 0;
+            List<int> numbers = new List<int>();
+            int indexOfAllInputs = 1;
+            List<string> allInputs = new List<string>();
+
+
+            while (true)
+            {
+                string singleInput;
+
+                Console.Write("\nEnter the value : ");
+                singleInput = Console.ReadLine();
+
+                allInputs.Add(singleInput);
+                bool isIntString = singleInput.All(char.IsDigit);
+
+                if (singleInput == "E")
+                {
+                    break;
+                }
+                else if (isIntString) 
+                {
+                    //int intNumber = int.TryParse(singleInput);
+                    int intNumber;
+                    bool success = int.TryParse(singleInput, out intNumber);
+                    if (success)
+                    {
+                        sum = sum + intNumber;
+                        numbers.Add(intNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The number '{singleInput ?? "<null>"}' is too big to process.");
+                    }
+
+                }
+                else
+                {
+                    Console.Write("Value could not be added : {0}\n", singleInput);
+                }
+
+                indexOfAllInputs++;
+            }
+
+            Console.Write("\n\nsum : {0}", sum);
+            Console.Write("\nall values : ");
+            for (int i = 0; i < allInputs.Count; i++)
+                Console.Write(allInputs[i] + " ");
+            Console.Write("\nNumbers : ");
+            for (int i = 0; i < numbers.Count; i++)
+                Console.Write(numbers[i] + " ");
+
+            return numbers;
+        }
+
+        static List<int> FindEvenNumbers(List<int> numbers)
+        {
+            //List<int> numbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int remainder = numbers[i] % 2;
+                if (remainder == 0)
+                {
+                    evenNumbers.Add(numbers[i]);
+                }
+            }
+
+            Console.Write("\nEven numbers : ");
+            for (int i = 0; i < evenNumbers.Count; i++)
+                Console.Write(evenNumbers[i] + " ");
+
+            return evenNumbers;
+        }
+
+    }
+
+}
+
