0f1121059ad8e5fc53cea2c73de7755f282dcb08
Hüma Bilgin <46404802+humabilgin@users.noreply.github.com>
2022-05-18 12:06:17 +0300

    Add files via upload

---
+++ IsFibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciExample
+{
+    public class Fibonacci
+    {
+        public void isFibonacci(List<int> evenNumbers)
+        {
+            int[] fibonacciNumbers = { 0, 2, 8 };
+            evenNumbers.Sort();
+            evenNumbers.Reverse();
+            Console.Write("\nSorted even numbers : ");
+            for (int i = 0; i < evenNumbers.Count; i++)
+                Console.Write(evenNumbers[i] + " ");
+
+            bool control = false;
+            for (int i = 0; i < 3; i++) {
+                if (fibonacciNumbers[i] == evenNumbers[0])
+                {
+                    Console.WriteLine("\n\nBiggest even number is a fibonacci number.");
+                    control = true;
+                }
+            }
+            if (!control)
+            {
+                Console.WriteLine("\n\nBiggest even number is not a fibonacci number.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibonacciExample
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> evenNumbers = new List<int>();
+            evenNumbers = IsEven();
+            Fibonacci fibonacci = new Fibonacci();
+            fibonacci.isFibonacci(evenNumbers);
+        }
+        static List<int> IOprocesses()
+        {
+
+            int controlKey = 0;
+            int sum = 0;
+            List<int> numbers = new List<int>();
+            string allValues = "";
+            while (controlKey == 0)
+            {
+
+                char ch;
+                Console.Write("\n\nEnter the value : ");
+                ch = Console.ReadKey().KeyChar;
+                //Console.WriteLine("\nchar is : {0}", ch);
+
+                allValues = allValues + ch.ToString();
+
+                if (ch == 'E')
+                {
+                    controlKey = 1;
+                }
+                else if (ch < 58 && ch > 47)
+                {
+                    int number = (int)Char.GetNumericValue(ch);
+                    sum = sum + number;
+                    numbers.Add(number);
+                }
+                else
+                {
+                    Console.Write("\nvalue could not be added : {0}", ch);
+                }
+            }
+
+            Console.Write("\n\nsum : {0}", sum);
+            Console.Write("\nall values : {0}\n", allValues);
+            Console.Write("Numbers : ");
+            for (int i = 0; i < numbers.Count; i++)
+                Console.Write(numbers[i] + " ");
+
+            return numbers;
+        }
+
+        static List<int> IsEven()
+        {
+            List<int> numbers = new List<int>();
+            numbers = IOprocesses();
+            List<int> evenNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int isEven = numbers[i] % 2;
+                if (isEven == 0)
+                {
+                    evenNumbers.Add(numbers[i]);
+                }
+            }
+
+            Console.Write("\nEven numbers : ");
+            for (int i = 0; i < evenNumbers.Count; i++)
+                Console.Write(evenNumbers[i] + " ");
+
+            return evenNumbers;
+        }
+
+    }
+
+}
+
