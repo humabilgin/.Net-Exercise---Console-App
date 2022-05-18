a25fb34a8f83032bfed6607e0f643cce087cf389
Hüma Bilgin <46404802+humabilgin@users.noreply.github.com>
2022-05-18 17:58:56 +0300

    Update Program.cs

--- fibonacciExample/fibonacciExample/Program.cs
+++ fibonacciExample/fibonacciExample/Program.cs
@@ -14,7 +14,7 @@ namespace fibonacciExample
             FibonacciHelper fibonacci = new FibonacciHelper();
 
             //flow
-            numbers = readInputAndFindNumbers();
+            numbers = ReadInputAndFindNumbers();
             evenNumbers = FindEvenNumbers(numbers);
             int biggestNumber = fibonacci.FindBiggestNumber(evenNumbers);
             bool isFibonacciNumber = fibonacci.IsFibonacci(biggestNumber);
@@ -29,7 +29,7 @@ namespace fibonacciExample
                 Console.WriteLine("\n\n************The biggest even number is not a Fibonacci number.************");
             }
         }
-        static List<int> readInputAndFindNumbers()
+        static List<int> ReadInputAndFindNumbers()
         {
 
             int sum = 0;
